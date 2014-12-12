973a106aee0eb3bdefdee6609bb65f61a8829b2d
Robert Krombholz <robertkrombholz@gmx.de>
2014-12-12 18:55:24 -0500

    *slim down the config file to only what is required

--- getch.groovy
+++ getch.groovy
@@ -1,4 +1,2 @@
 getch.base.directory = '/data/getch'
 getch.encryption.password = System.getenv()['ENCRYPTION_PASSWORD']
-getch.trusted.proxies = [ '127.0.0.1' ]
-getch.feature.templating.enabled = true
